c3a602efc8a66b49fb17b64ec51e9bef921a4ce4
marianyarj <marianyarj@gmail.com>
2025-11-17 11:17:13 +0100

    chore: add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/space-mono/400.css'
+import '@fontsource/space-mono/700.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
